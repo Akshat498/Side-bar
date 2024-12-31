9449abd90b56b92f1e3889b0f6dd0618a099b6fc
akshat498 <akshatdhukia23@gmail.com>
2024-12-31 11:52:20 +0530

    add layer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 const toggleSidebar = (open) => {
     document.getElementById('sidebar').classList.toggle('open', open);
-    document.getElementById('overlay').classList.toggle('show', !open);
+    document.getElementById('upper-layer').classList.toggle('show', !open);
 };
-['fnc-buton', 'close-buton', 'overlay'].forEach((id, i) =>
+['fnc-buton', 'close-buton', 'upper-layer'].forEach((id, i) =>
     document.getElementById(id).addEventListener('click', () => toggleSidebar(i !== 0)));
